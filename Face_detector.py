525f52eb6db132b6dde1cedcfec1ceb84cd36b7a
N0H3dy <workmail2776@gmail.com>
2022-09-17 17:02:12 -0400

    Completed live webcam AI face detection software

--- Face_detector.py
+++ Face_detector.py
@@ -4,23 +4,32 @@ import cv2
 trained_face_data = cv2.CascadeClassifier('C:\Python310\Lib\site-packages\cv2\data\haarcascade_frontalface_default.xml')
 
 # the photo where the programm will detect the face
-img = cv2.imread('olimpia_stage.jpg')
+# img = cv2.imread('olimpia_stage.jpg')
 
-#Converts image to grayscale (so that it ist a headache to make the computer detect the face)
-grayscaled_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+webcam = cv2.VideoCapture(0)
 
-#detect face cordinates
-face_cordinates = trained_face_data.detectMultiScale(grayscaled_img)
+while True:
+    #read current frame
+    successful_fram_read, frame = webcam.read()
 
-#draw rectangle around the face(s)
-for (x,y,z,h) in face_cordinates:
-    cv2.rectangle(img, (x,y), (x+z, y+h), (0, 255, 0), 2)
+    #Converts image to grayscale (so that it ist a headache to make the computer detect the face)
+    grayscaled_img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-print(face_cordinates)
+    #detect face cordinates
+    face_cordinates = trained_face_data.detectMultiScale(grayscaled_img)
 
-#opens the photo with the face in it
-cv2.imshow('face detoctor', img)
-#pauses the programm so that the image does not imediatley disappear
-cv2.waitKey()
+    #draw rectangle around the face(s)
+    for (x,y,z,h) in face_cordinates:
+        cv2.rectangle(frame, (x,y), (x+z, y+h), (0, 255, 0), 2)
+
+    cv2.imshow('face detoctor', frame)
+    #pauses the programm so that the image does not imediatley disappear
+    key = cv2.waitKey(1)
+
+    #if you press q key then quit the programm
+    if key==81 or key==113:
+        break
+
+    webcam.release()
 
 print("Code Completed")
